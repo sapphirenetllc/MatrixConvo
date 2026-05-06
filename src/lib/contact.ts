17310d267ae385efdb15488b7dbbff7385c68dd7
sapphirenetllc <sapphirenetllc@gmail.com>
2026-05-06 17:46:32 +0530

    Update primary phone number to (866) 867-5320

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,5 +1,5 @@
-export const CONTACT_PHONE_DISPLAY = "+1 (604) 332-6360";
-export const CONTACT_PHONE_TEL = "+16043326360";
+export const CONTACT_PHONE_DISPLAY = "+1 (866) 867-5320";
+export const CONTACT_PHONE_TEL = "+18668675320";
 export const CONTACT_PHONE_DISPLAY_2 = "+1 (604) 239-7097";
 export const CONTACT_PHONE_TEL_2 = "+16042397097";
 export const CONTACT_PHONES = [
